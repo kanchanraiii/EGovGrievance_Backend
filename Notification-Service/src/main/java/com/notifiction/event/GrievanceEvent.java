9ab37e7a0a1acb6db896eb1a631fee346d9692de
kanchanraiii <kanchanrai2307@gmail.com>
2026-01-04 12:31:40 +0530

    removed duplications

--- Notification-Service/src/main/java/com/notifiction/event/GrievanceEvent.java
+++ Notification-Service/src/main/java/com/notifiction/event/GrievanceEvent.java
@@ -10,30 +10,4 @@ public class GrievanceEvent {
     private String message;
     private String eventType;  // SUBMITTED / ASSIGNED / ESCALATED / RESOLVED
 	
-    // getters and setters
-    public String getGrievanceId() {
-		return grievanceId;
-	}
-	public void setGrievanceId(String grievanceId) {
-		this.grievanceId = grievanceId;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getEventType() {
-		return eventType;
-	}
-	public void setEventType(String eventType) {
-		this.eventType = eventType;
-	}
-     
 }
